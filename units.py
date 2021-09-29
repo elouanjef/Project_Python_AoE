82a3dfd0ae0a2127c20bb4566cb41a8ee699fb74
elouanjef <82178294+elouanjef@users.noreply.github.com>
2021-09-29 22:48:21 +0200

    Update classe pour unités et bâtiments

--- units.py
+++ units.py
@@ -1,52 +1,5 @@
 from typing import Dict
 
-
-class Unit:
-
-    alive = False
-    type = False
-    name = False
-    costw = 0
-    costf = 0
-    costg = 0
-    costr = 0
-
-    def __init__(self, name, type):
-        self.name = name
-        self.type = type
-        assert self.type in ("cavalerie","infanterie","artillerie"), self.type
-        print("Vous avez enrôlé un(e)",str(self.name,"de type",str(self.type)))
-        Unit.alive = True
-
-    def stats(self,costw,costf,costg,costr):
-        self.costw = costw
-        self.costf = costf
-        self.costg = costg
-        self.costr = costr
-
-class Batiment:
-
-    alive = True
-    type = False
-    name = False
-    costw = 0
-    costf = 0
-    costg = 0
-    costr = 0
-
-    def __init__(self, name, type):
-        self.name = name
-        self.type = type
-        assert self.type in ("militaire", "economie"), self.type
-        print("Vous avez bâti un(e)",str(self.name),"de type",str(self.type))
-        Batiment.alive = True
-
-    def stats(self,costw,costf,costg,costr):
-        self.costw = costw
-        self.costf = costf
-        self.costg = costg
-        self.costr = costr
-
 class Unite:
 
     batiment = False
