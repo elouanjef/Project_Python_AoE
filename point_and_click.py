03ec9d77b883b0d3a1c9cbac8efb360a8a6931b7
TTSiWOOD <nguyencongkhai2k1vl@gmail.com>
2021-11-01 20:52:08 +0100

    fini d'ecrire point and click

--- point_and_click.py
+++ point_and_click.py
@@ -1,100 +1,66 @@
 import pygame as pg
-from pygame.constants import MOUSEBUTTONDOWN
-from math import  acos, sqrt, cos, sin
-pg.init()
 
-clock = pg.time.Clock()
-current_time = 0
-win = pg.display.set_mode((0, 0), pg.NOFRAME)
-pg.display.toggle_fullscreen()
-#win = pg.display.set_mode((1000, 500))
 
-pg.display.set_caption("First Game")
-class character:
-    def __init__(self):
-        self.x = 50
-        self.y = 50
-        self.radius = 5
-        self.step_size = 5
-        self.color = (150, 150, 150)
-        self.tempx = 0
-        self.tempy = 0
+class Ship():
 
-    #this will return the angle between 2 line: the line from player position and to the end point and the horizontal line
-    def findDegrees(self,start_x,start_y,target_x,target_y):
-        if target_x!=start_x and target_y!= start_y:
-            a=sqrt((target_x-start_x)*(target_x-start_x)+(target_y-start_y)*(target_y-start_y))
-            b=10 #  a random number
-            c=sqrt((target_x-start_x-10)*(target_x-start_x-10)+(target_y-start_y)*(target_y-start_y))
-            if target_y <=start_y :
-                return acos((a * a + b * b - c * c)/(2.0 * a * b))
-            else:
-                return -acos((a * a + b * b - c * c)/(2.0 * a * b))
-    #Capture mouse click position
-    # def mouse_click(self):
-    #   x,y = pg.mouse.get_pos()
-    #   return x,y
+    def __init__(self, speed, color):
+        self.image = pg.Surface((10, 10))
+        self.image.set_colorkey((12,34,56))
+        self.image.fill((12,34,56))
+        pg.draw.circle(self.image, color, (5, 5), 5)
+        self.rect = self.image.get_rect()
+        self.color = color
 
-    def move(self, target_x, target_y):
-        moving = True
-        if abs(target_x-self.x)<10**(-3): 
-            velocity_x=0
-        else: 
-            velocity_x=(self.step_size*abs(target_x-self.x))/sqrt(abs(target_x-self.x)*abs(target_x-self.x)+abs(target_y-self.y)*abs(target_y-self.y))
-        if abs(target_y-self.y)<10**(-3): 
-            velocity_y=0
-        else: 
-            velocity_y=(self.step_size*abs(target_y-self.y))/sqrt(abs(target_x-self.x)*abs(target_x-self.x)+abs(target_y-self.y)*abs(target_y-self.y))
-        while moving:
-            if not(target_x - velocity_x < self.x and self.x < target_x + velocity_x):
-                if target_x >= self.x:
-                    self.x += velocity_x
-                else:
-                    self.x -= velocity_x
+        self.pos = pg.Vector2(0, 0)
+        self.set_target((0, 0))
+        self.speed = speed
 
-            if not(target_y-velocity_y < self.y and self.y < target_y + velocity_y):
-                if target_y >= self.y:
-                    self.y+=velocity_y
-                else:
-                    self.y-=velocity_y
-            if((abs(target_x-self.x) < self.step_size)and (abs(target_y-self.y) < self.step_size)):
-                moving=False
-            pg.time.delay(100)
-            win.fill((0, 0, 0))
-            pg.draw.circle(win, crts.color, (crts.x, crts.y), crts.radius)
-            pg.display.update()
-            print("position: (%f, %f)" % (self.x,self.y))
+    def set_target(self, pos):
+        self.target = pg.Vector2(pos)
 
-    def cha_move(self):
-        pass
-            
+    def draw(self,screen):
+        screen.fill((0, 0, 0))
+        pg.draw.circle(screen, self.color , (self.pos[0], self.pos[1]), 5)
+        pg.display.update()
 
-    
-crts = character()
-run = True
-while run:
-    #pg.time.delay(200)
+    def update(self):
+        move = self.target - self.pos
+        move_length = move.length()
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            run = False
-            pg.quit()
-        if event.type == pg.KEYDOWN:
+        if move_length < self.speed:
+            self.pos = self.target
+        elif move_length != 0:
+            move.normalize_ip()
+            move = move * self.speed
+            self.pos += move
+
+        self.rect.topleft = list(int(v) for v in self.pos)
+
+def main():
+    pg.init()
+    quit = False
+    # screen = pg.display.set_mode((1000, 500))
+    screen = pg.display.set_mode((0, 0), pg.NOFRAME)
+    pg.display.toggle_fullscreen()
+    clock = pg.time.Clock()
+
+    group = Ship(1.5, pg.Color('white'))
+
+    while not quit:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return 
+            if event.type == pg.MOUSEBUTTONDOWN:
+                group.set_target(pg.mouse.get_pos())
+            if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
-                    run = False
-                    #pg.quit()
+                    pg.quit()
 
-    #keys = pg.key.get_pressed()
-    if event.type == MOUSEBUTTONDOWN:
-        x,y = pg.mouse.get_pos()
-        crts.move(x,y)
+        group.update()
+        screen.fill((20, 20, 20))
+        group.draw(screen)
+        pg.display.flip()
+        clock.tick(60)
 
-        
-    win.fill((0, 0, 0))
-
-    pg.draw.circle(win, crts.color, (crts.x, crts.y), crts.radius)
-    pg.display.update()
-
-
-
-pg.quit()
+if __name__ == '__main__':
+    main()
